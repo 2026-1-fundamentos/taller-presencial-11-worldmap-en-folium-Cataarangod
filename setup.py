fe384ddf043cbf5652bcb6356036993e4266c06e
Catalina Arango <75914408+Cataarangod@users.noreply.github.com>
2026-07-02 00:07:37 -0500

    test: homework

--- setup.py
+++ setup.py
@@ -8,8 +8,9 @@ setup(
     packages=find_packages(),
     install_requires=[
         "pytest",
-        "matplotlib",
         "pandas",
-        "folium",
+        "scikit-learn",
+        "ipykernel",
+        "xlrd",
     ],
 )
